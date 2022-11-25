266a74381e21711ad249f6b174697b4b227a1cd8
tomoyaok1113 <tomoya001113@g.ecc.u-tokyo.ac.jp>
2022-11-25 21:12:51 +0900

    A

--- hackathon-be/add_point.go
+++ hackathon-be/add_point.go
@@ -74,14 +74,8 @@ func handlerPoint(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		userpoint, err := db.Query("SELECT SUM(point) FROM messagelist WHERE toname=?", v.ToName)
-		if err != nil {
-			log.Printf("fail: db.fetchPoint, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
 
-		_, err = tx.Exec("UPDATE userlist SET point=? WHERE name=?", userpoint, v.ToName)
+		_, err = tx.Exec("UPDATE userlist SET point=100 WHERE name=?", v.ToName)
 		if err != nil {
 			tx.Rollback()
 			log.Printf("fail: db.Prepare, %v\n", err)
