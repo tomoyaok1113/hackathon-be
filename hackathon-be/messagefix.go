91b40189c0fc8c5130dcfb61aad9cd827a12e7ab
tomoyaok1113 <tomoya001113@g.ecc.u-tokyo.ac.jp>
2022-11-26 13:13:26 +0900

    lastcommit

--- hackathon-be/messagefix.go
+++ hackathon-be/messagefix.go
@@ -60,6 +60,16 @@ func handlerFixMessage(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%v\n", userpoint)
 		userpoint = userpoint - messagepoint
 		_, err = tx.Exec("UPDATE userlist SET point=? WHERE name=?", userpoint, v.Fixname)
+
+		point := 0
+		if err := tx.QueryRow("SELECT point FROM userlist WHERE name = ?", v.ToName).Scan(&point); err != nil {
+			tx.Rollback()
+			log.Printf("fail: db.Prepare, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		point = point + v.Point
+		_, err = tx.Exec("UPDATE userlist SET point=? WHERE name=?", point, v.ToName)
 		_, err = tx.Exec("UPDATE messagelist SET toname=?, point=?, message=? WHERE id=?", v.ToName, v.Point, v.Message, v.Id)
 		if err != nil {
 			tx.Rollback()
